fffd19f698dcb4d04e51c87365e4076783ce9f91
AMsmidt <anne-mette4@hotmail.com>
2020-02-04 18:13:44 +0100

    Update db-con.js

--- db-con.js
+++ db-con.js
@@ -1,39 +1,3 @@
-/*let mysql = require('mysql');
-
-let connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "dkkdb"
-});
-
-connection.connect(function(err) {
-    if (err) {
-      return console.error('error: ' + err.message);
-    }
-   
-    console.log('Connected to the MySQL server.');
-  });*/
-
- /* var mysql = require('mysql');
-
- var connection = mysql.createConnection({
-  host     : '127.0.0.1',
-  port     : '3306',
-  user     : 'root',
-  password : 'password',
-  database : 'dkkdb'
- });
-
- connection.connect( function(err){
-if (err){ 
-    throw err;
-}
-else {
-    console.log('Connected');
-}
- });*/
-
 let mysql = require('mysql');
   
   const con = mysql.createConnection({
